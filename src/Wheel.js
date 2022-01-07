6ffdcfbd941896bf254721c8ecf806a8c7f6bcec
georgemaganzini <hyped@teamliquid.com>
2022-01-07 12:42:58 -0800

    Add working base for wheel menu

--- src/Wheel.js
+++ src/Wheel.js
@@ -1,71 +1,75 @@
 import React from 'react';
 import oMenu from 'o-menu';
+import { useEffect } from 'react';
 
 function Wheel(props) {
-    const omenu = oMenu('main-div', {
-    menu: {
-        innerCircleRadius: 55
-    },
-    nthSlice: {
-        contentSize: 22,
-        iconDistanceFromInnerCircle: 5,
-        parentFillMode: -0.3,
-        styles: {
-            contentContainer:{
-                fontSize: 22,
-                color: '#efefef'
-            }
-        }
-    },
-    slice: {
-        contentSize : 30,
-        iconDistanceFromInnerCircle: 10,
-        styles: {
-            contentContainer:{
-                fontSize: 30,
-                color: '#efefef'
-            },
-        }
-    },
-    onEndCloseAnimation: val => {
-        if(val)
-            alert(`You want to: ${val}`)
-    }
-});
+	useEffect(() => {
+		const omenu = oMenu('app', {
+			menu: {
+				innerCircleRadius: 55,
+			},
+			nthSlice: {
+				contentSize: 22,
+				iconDistanceFromInnerCircle: 5,
+				parentFillMode: -0.3,
+				styles: {
+					contentContainer: {
+						fontSize: 22,
+						color: '#efefef',
+					},
+				},
+			},
+			slice: {
+				contentSize: 30,
+				iconDistanceFromInnerCircle: 10,
+				styles: {
+					contentContainer: {
+						fontSize: 30,
+						color: '#efefef',
+					},
+				},
+			},
+			// onOpen: onOpenCb,
+			onEndCloseAnimation: (val) => {
+				if (val) alert(`You want to: ${val}`);
+			},
+		});
 
-omenu.on('sliceEnter', ev => {
-    console.log(ev); // => oMenu Event
-});
+		omenu.on('sliceEnter', (ev) => {
+			console.log(ev); // => oMenu Event
+		});
 
-// document.body.addEventListener('contextmenu', ev => {
-//     ev.preventDefault();
+		document.body.addEventListener('click', (ev) => {
+			ev.preventDefault();
 
-    // omenu.open(ev, {
-    //    slices :[
-    //        {
-    //            content: 'A',
-    //            styles:{
-    //                defaults: {
-    //                    fill: '#8BC34A'
-    //                }
-    //            },
-    //            data: 'send email'
-    //        },
-    //        {
-    //            content: 'B',
-    //            styles:{
-    //                defaults: {
-    //                    fill: '#F44336'
-    //                }
-    //            },
-    //            data: 'delete user'
-    //        }
-    //    ]
-    // });
+			omenu.open(ev, {
+				slices: [
+					{
+						content: 'A',
+						styles: {
+							defaults: {
+								fill: '#8BC34A',
+							},
+						},
+						data: 'send email',
+					},
+					{
+						content: 'B',
+						styles: {
+							defaults: {
+								fill: '#F44336',
+							},
+						},
+						data: 'delete user',
+					},
+				],
+			});
+		});
 
+	}, []);
 
 	return (
-		<div id='main-div'>
+		<div id='app'>
 			<div>hi</div>
 		</div>
 	);

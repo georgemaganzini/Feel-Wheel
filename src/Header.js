714efbc7cafec62a9392bc3ec4ec24f64dd615c9
george2 <hyped@teamliquid.com>
2022-03-09 09:34:12 -0800

    Edit Header, add favicon

--- src/Header.js
+++ src/Header.js
@@ -5,8 +5,8 @@ function Header({ handleChange, handleSubmit, searchString }) {
 	return (
 		<div className='header'>
 			<h1>Welcome to the Feel Wheel</h1>{' '}
-			<h3>Right-Click Anywhere to Start, or Use the Search</h3>
-			<h5>(Long Press on Mobile)</h5>
+			<h3>Right-Click Anywhere to Start, Left Click to Save URL</h3>
+			<h5>(Long Press on Android)</h5>
 			<Search
 				handleChange={handleChange}
 				handleSubmit={handleSubmit}
